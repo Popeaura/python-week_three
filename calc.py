d99ba032372bf4f676300d65939032c94451386f
Popeaura <popeahura@gmail.com>
2024-11-13 15:58:17 +0300

    I added and modified this file to fit my assignments requirements

--- calc.py
+++ calc.py
@@ -3,4 +3,9 @@ def calculate_discount(price, discount_percentage):
     if discount_percentage >= 20:
         #calculate the discount amount
         discount_amount= price *(discount_percentage/ 100)
-        
+        #calculate the final price after applying the discount
+        final_price = price - discount_amount
+        return final_price
+    else:
+        #return the original price if discount is less than 20%
+        return price
